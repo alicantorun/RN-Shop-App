b2ec881defa5fcfe083e24ee28f948b6ecbea755
Alican <alican@Alican-MacBook-Pro.local>
2019-09-15 17:22:27 +0300

    inputs handled

---
+++ screens/shop/OrdersScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { View, Text, StyleSheet, FlatList, Platform } from "react-native";
+import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../../components/UI/HeaderButton";
+import OrderItem from "../../components/shop/OrderItem";
+
+const OrdersScreen = props => {
+  const orders = useSelector(state => state.orders.orders);
+  console.log(orders);
+  return (
+    <FlatList
+      data={orders}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <OrderItem
+          amount={itemData.item.totalAmount}
+          date={itemData.item.readableDate}
+          items={itemData.item.items}
+        />
+      )}
+    />
+  );
+};
+
+OrdersScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Your Orders",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Cart"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  };
+};
+const styles = StyleSheet.create({});
+
+export default OrdersScreen;
